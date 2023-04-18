c95c4c0319037b9ebe45d6b72ff288b1dc9d8154
TaraCrane <78365426+TaraCrane@users.noreply.github.com>
2023-04-18 13:43:36 +0100

    Update form.py

--- form.py
+++ form.py
@@ -9,7 +9,7 @@ streamlit.header("Clients:")
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT client_name FROM clients")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
