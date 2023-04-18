2149d421932c112fec09e1f069d389bb34a8c060
TaraCrane <78365426+TaraCrane@users.noreply.github.com>
2023-04-18 14:47:04 +0100

    Update form.py

--- form.py
+++ form.py
@@ -14,9 +14,9 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select client_name from clients")
 my_data_rows = my_cur.fetchall()
-streamlit.dataframe(my_data_rows)
+client_list = streamlit.dataframe(my_data_rows)
 
-client_selected = streamlit.multiselect("Pick some fruits:", my_data_rows)
+client_selected = streamlit.multiselect("Pick some fruits:", client_list)
 
 
 # client_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
