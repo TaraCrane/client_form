05d63843a68954c27245a7d45aa75e4477e3adfe
TaraCrane <78365426+TaraCrane@users.noreply.github.com>
2023-04-18 13:26:49 +0100

    Update form.py

--- form.py
+++ form.py
@@ -4,10 +4,10 @@ import snowflake.connector
 streamlit.title('Client Form')
 streamlit.header('Project Data Collection')
 
-  # Connect to Snowflake
-streamlit.header("Clients:")
-# Snowflake related functions
-def get_client_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("SELECT * FROM clients")
-         return my_cur.fetchall()
+#   # Connect to Snowflake
+# streamlit.header("Clients:")
+# # Snowflake related functions
+# def get_client_list():
+#     with my_cnx.cursor() as my_cur:
+#          my_cur.execute("SELECT * FROM clients")
+#          return my_cur.fetchall()
