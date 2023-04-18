f1d434e1cc3a976ae7d235ec37db64bebe77da30
TaraCrane <78365426+TaraCrane@users.noreply.github.com>
2023-04-18 13:41:44 +0100

    Update form.py

--- form.py
+++ form.py
@@ -6,8 +6,32 @@ streamlit.header('Project Data Collection')
 
   # Connect to Snowflake
 streamlit.header("Clients:")
-# Snowflake related functions
-def get_client_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("SELECT client_name FROM clients")
-         return my_cur.fetchall()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT client_name FROM clients")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+# # Snowflake related functions
+# def get_client_list():
+#     with my_cnx.cursor() as my_cur:
+#          my_cur.execute("SELECT client_name FROM clients")
+#          return my_cur.fetchall()
+        
+#   # Add a pick list to pick the client
+# client_selected = streamlit.select("Pick a client:", list(get_client_list.index))
+# client_to_show = get_client_list.loc[client_selected]
+        
+# try:
+#   client_choice = streamlit.text_input('What client would you like information about?')
+#   if not client_choice:
+#       streamlit.error("Please select a client to get information.")
+#   else:
+#     back_from_function = get_client_list(client_choice)
+#     streamlit.dataframe(back_from_function)
+# except URLError as e:
+#     streamlit.error()
+# streamlit.write('The user entered ', fruit_choice)
+        
