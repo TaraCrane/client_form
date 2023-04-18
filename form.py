107af1911ecac61775bfd22ca6217b711cd27cae
TaraCrane <78365426+TaraCrane@users.noreply.github.com>
2023-04-18 14:45:54 +0100

    Update form.py

--- form.py
+++ form.py
@@ -16,7 +16,8 @@ my_cur.execute("select client_name from clients")
 my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
 
-streamlit.list(my_data_rows)
+client_selected = streamlit.multiselect("Pick some fruits:", my_fruit_list)
+
 
 # client_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # client_list = my_data_rows.set_index('client_name')
